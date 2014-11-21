13c296800de826b144db1bf29a81a2e6c1906f49
Bob Hall <RobertKHall@gmail.com>
2014-11-21 10:46:45 -0800

    fix degrees_to_cardinal, cast degrees as number not string

--- wind_utils.js
+++ wind_utils.js
@@ -2,5 +2,6 @@
 var wind_directions = ['N','NNE','NE','ENE','E','ESE','SE','SSE','S','SSW','SW','WSW','W','WNW','NW','NNW'];
 
 exports.degrees_to_cardinal = function(degrees) {
+  degrees = parseInt(degrees);
   return wind_directions[Math.floor(((degrees+11.25) % 360) / 22.5 )];
 };
